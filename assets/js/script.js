55bf185b0d3fd4a99cca812f5f46256721666b39
Anastasia Perez <Stasiperez85@gmail.com>
2022-01-28 21:27:14 -0700

    updating feature code

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,16 @@ $(document).ready(function () {
         localStorage.setItem(zip, text)
     })
 });
+
+
+    var urlSearch = "https://api.themoviedb.org/3/movie/now_playing?api_key=8c898a456ab85fa46fb53684097389dc&language=en-US&region=US"
+
+    fetch(urlSearch).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          console.log(data);
+        })
+      }
+    });
+
+    
