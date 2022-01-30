3a51a10e20e9ee9858e49e9d44982c05215d7ea6
Matthew Kelsch <95149182+mkelsch2000@users.noreply.github.com>
2022-01-29 20:23:44 -0700

    switch to working on movie API, and finalized it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
-var apiUrl = "https://api.tomtom.com/search/2/geocode/Saint+George+UT+94790.sjon?key=AJyORkvIskjy38hOMUwmZeWJdZB2HIEt";
 
-fetch(apiUrl).then(function(response) {
+// fetch movie data
+fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=8c898a456ab85fa46fb53684097389dc&language=en-US&region=US")
+
+.then(function(response) {
   if (response.ok) {
     response.json().then(function(data) {
-      console.log(data);
+      const list = data.results
+      console.log(data.results);
+
+      for (var i = 0; i < list.length; i++) {
+        const movieName = list[i].title;
+        const moviePoster = list[i].poster_path
+        const movieDiv = document.createElement("div");
+        movieDiv.classList.add("movies", "column", "is-1", "is-mobile");
+        movieDiv.innerHTML = "<h2>" + movieName + "</h2><img src='https://image.tmdb.org/t/p/original" + moviePoster + "'/>";
+        document.getElementById("carousel-demo").appendChild(movieDiv);
+      }
     })
   }
-})
-
-
-
-
-
-
+});
