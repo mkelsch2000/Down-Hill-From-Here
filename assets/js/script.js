9d65127f602970e9ca270258136ef55304dc6561
haybales <haleynicole126@gmail.com>
2022-01-30 18:37:24 -0700

    movie data inside carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,16 @@ fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=8c898a456ab85fa46f
         const movieName = list[i].title;
         const moviePoster = list[i].poster_path
         const movieDiv = document.createElement("div");
-        movieDiv.classList.add("movies", "column", "is-1", "is-mobile");
-        movieDiv.innerHTML = "<h2>" + movieName + "</h2><img src='https://image.tmdb.org/t/p/original" + moviePoster + "'/>";
-        document.getElementById("carousel-demo").appendChild(movieDiv);
+        movieDiv.classList.add("movies", "column", "is-1", "is-mobile", "content");
+        movieDiv.innerHTML = "<img src='https://image.tmdb.org/t/p/original" + moviePoster + "'/><h2>" + movieName + "</h2>";
+        document.getElementById("movie-card").appendChild(movieDiv);
       }
     })
   }
 });
 
 // Initialize all div with carousel class
-var carousels = bulmaCarousel.attach('.carousel', options);
+var carousels = bulmaCarousel.attach('.carousel');
 
 // Loop on each carousel initialized
 for(var i = 0; i < carousels.length; i++) {
