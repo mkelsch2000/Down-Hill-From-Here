fab428faafb9fcc4323a9f2e20e350c2c64d675d
Anastasia Perez <Stasiperez85@gmail.com>
2022-01-31 21:53:19 -0700

    save api storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=8c898a456ab85fa46f
       response.json().then(function (data) {
         const list = data.results
         console.log(data.results);
-      
+
         localStorage.setItem("list", JSON.stringify(list));
 
         JSON.parse(localStorage.getItem("list"));
