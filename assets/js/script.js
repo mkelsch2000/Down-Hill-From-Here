0a7ee83388e1c5aa31b6840bb394585bf901c9b4
Matthew Kelsch <95149182+mkelsch2000@users.noreply.github.com>
2022-01-31 20:25:22 -0700

    finalized map flexability/functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ var getRequestedData = function(event) {
   mapContainer.innerHTML = "";
   mapContainer.innerHTML = "<iframe class='google-map mx-auto' width='400' height='600' style='border:0' loading='lazy' allowfullscreen src='https://www.google.com/maps/embed/v1/search?q=movie+theater+near+" + zipEl + "&key=AIzaSyAnFqpH_kWqpIqDBT_nOesR9gTnPS9mIxg' title='description'></iframe>"
 };
+document.getElementById("search-Btn").addEventListener("click", getRequestedData);
 
 
 // fetch movie data
@@ -33,25 +34,4 @@ fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=8c898a456ab85fa46f
   }
 });
 
-// Initialize all div with carousel class
-// var carousels = bulmaCarousel.attach('.carousel', options);
-
-// Loop on each carousel initialized
-// for(var i = 0; i < carousels.length; i++) {
-// 	// Add listener to  event
-// 	carousels[i].on('before:show', state => {
-// 		console.log(state);
-// 	});
-// }
-
-// Access to bulmaCarousel instance of an element
-// var element = document.querySelector('#my-element');
-// if (element && element.bulmaCarousel) {
-// 	// bulmaCarousel instance is available as element.bulmaCarousel
-// 	element.bulmaCarousel.on('before-show', function(state) {
-// 		console.log(state);
-// 	});
-// }
-
-document.getElementById("search-Btn").addEventListener("click", getRequestedData);
 
