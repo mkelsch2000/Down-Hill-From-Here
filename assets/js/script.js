da810714947d32775bfdd9c9edb022c968c9f3ce
Anastasia Perez <Stasiperez85@gmail.com>
2022-01-31 18:30:05 -0700

    save list from API to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-// selecting elements form the DOM
-const searchButton = document.querySelector("#searchbtn");
-
-// activate search button to populate data
-searchButton.onclick = function(event) {
-    
-
 // fetch movie data
 fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=8c898a456ab85fa46fb53684097389dc&language=en-US&region=US")
 
@@ -14,38 +7,23 @@ fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=8c898a456ab85fa46f
       const list = data.results
       console.log(data.results);
 
+
+      localStorage.setItem("list", JSON.stringify(list));
+
+      JSON.parse(localStorage.getItem("list"));
+
       for (var i = 0; i < list.length; i++) {
         const movieName = list[i].title;
         const moviePoster = list[i].poster_path
         const movieDiv = document.createElement("div");
         movieDiv.classList.add("movies", "column", "is-1", "is-mobile");
         movieDiv.innerHTML = "<h2>" + movieName + "</h2><img src='https://image.tmdb.org/t/p/original" + moviePoster + "'/>";
-        document.getElementById("carousel-demo").appendChild(movieDiv);
+        document.getElementById("carousel").appendChild(movieDiv);
       }
     })
   }
 });
-}
 
-// Initialize all div with carousel class
-var carousels = bulmaCarousel.attach('.carousel');
-
-// Loop on each carousel initialized
-for(var i = 0; i < carousels.length; i++) {
-	// Add listener to  event
-	carousels[i].on('before:show', state => {
-		console.log(state);
-	});
-}
-
-// Access to bulmaCarousel instance of an element
-var element = document.querySelector('#my-element');
-if (element && element.bulmaCarousel) {
-	// bulmaCarousel instance is available as element.bulmaCarousel
-	element.bulmaCarousel.on('before-show', function(state) {
-		console.log(state);
-	});
-}
 
 // fetch data from input field
 var getRequestedData = function(event) {
@@ -71,13 +49,25 @@ var getRequestedData = function(event) {
 
         localStorage.setItem(zip, text)
     })
-
-    $(".btn").on("onload", function () {
-
-    
-        var zip = $(this).parent().attr("id");
-        var text = $(this).siblings(".form-input").val();
-
-        localStorage.setItem(zip, text)
-    })
 });
+
+
+// // Initialize all div with carousel class
+// var carousels = bulmaCarousel.attach('.carousel');
+
+// // Loop on each carousel initialized
+// for(var i = 0; i < carousels.length; i++) {
+// 	// Add listener to  event
+// 	carousels[i].on('before:show', state => {
+// 		console.log(state);
+// 	});
+// }
+
+// // Access to bulmaCarousel instance of an element
+// var element = document.querySelector('#my-element');
+// if (element && element.bulmaCarousel) {
+// 	// bulmaCarousel instance is available as element.bulmaCarousel
+// 	element.bulmaCarousel.on('before-show', function(state) {
+// 		console.log(state);
+// 	});
+// }
